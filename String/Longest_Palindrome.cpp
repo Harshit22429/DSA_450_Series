522bc56da7258fb0abe58b13e9e6d1ee7f9e61af
Harshit22429 <singhtqp22429@gmail.com>
2023-07-28 15:44:19 +0530

    Logest Palindrome in string

---
+++ String/Longest_Palindrome.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+string longestPalindrome(string str){
+    string ans;
+    // int start =0;
+    int end = 0;
+    // int low, high;
+    for(int i =1; i<str.length(); i++){
+
+        // for odd
+        int low =i;
+        int high= i;
+        while(low>=0 && high<str.length() && str[low]==str[high]){
+            if(end<high-low+1){
+                ans = str.substr(low, high-low+1);
+                end = high-low+1;
+
+            }
+            low--; 
+            high++;
+        }
+
+        /// for even 
+        low= i-1;
+        high=i;
+        while(low>=0 && high<str.length() && str[low]==str[high]){
+            if(end<high-low+1){
+                ans = str.substr(low, high-low+1);
+                end = high-low+1;
+            }
+            low--;
+            high++;
+        }
+
+    }
+    return ans;
+
+}
+int main(){
+
+    string str ="aaaabbaa";
+
+    cout<<"longest palindrome in this string is : "<<longestPalindrome(str);
+}
--- rough.cpp
+++ rough.cpp
@@ -5,6 +5,29 @@ using namespace std;
 
 int main(){
 
-    int count = 2;
-    cout<<( int('0'))<<endl;
+    string str="abc";
+    for (int i =0; i<str.length(); i++){
+        int start =i;
+        int end = str.length()-1;
+        // cout<<str[i]<<endl;
+        while(start<end){
+            if(str[start]==str[end]){
+                start++;
+                end--;
+            }
+            else{
+                break;
+            }
+        }
+        if(start>=end){
+            if(i==str.length()-1){
+                cout<<"if complete last "<<"0"<<endl;
+            }
+            else{
+
+            cout<<"if complete "<<i<<endl;
+            }
+        break;
+        }
+    }
 }
